87b07f5fe3d6eb079d7ecb34b9cd56c05b4b0a5e
Ezra <ezradudden@gmail.com>
2020-06-17 11:05:30 -0600

    Update to add more robust error checking and handling in preparation for deployment with server. To work on testing (current coverage 60%)

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::time::Instant;
 use rand::Rng;
-const STRIDE: i64 = 1000;
 
 // The searching Algorithm
 fn balanced_partition(root: i64) -> Vec<i64> {
@@ -55,7 +54,7 @@ fn is_unique_subset(sol: [i128;9], potential: &[i128] ) -> bool {
 
 // Validates a potential set.
 fn validate_set(potentials :Vec<i64>)-> String {
-    let mut expanded = expand(&potentials);
+    let expanded = expand(&potentials);
     let mut i = 1;
     while i < expanded.len() {
        let mut j = i + 2;
@@ -107,8 +106,8 @@ fn compute_square(root: i128, x: i128, y: i128 ) -> [i128; 9] {
 fn run_thread(start: i64, end: i64, offset: i64, stepsize: i64, tx: Sender<String>){
     let mut i = start + offset;
     let mut c = 0;
-    let mut timer = Instant::now();
-       while i < end {
+    let timer = Instant::now();
+    while i < end {
         let p = balanced_partition(i);
         if p.len() >= 9 {
             let s = validate_set(p);
@@ -118,13 +117,10 @@ fn run_thread(start: i64, end: i64, offset: i64, stepsize: i64, tx: Sender<Strin
         }
         c = c + 1;
         i = i + stepsize;
-        if c % STRIDE == 0 {
-            let s = format!("i={} o={} t={}", i, offset, timer.elapsed().as_millis());
-            timer = Instant::now();
-            c = 0;
-            tx.send(s).unwrap();
-        }
+
     }
+    let s = format!("c={} i={} o={} t={}", c, i, offset, timer.elapsed().as_millis());
+    tx.send(s).unwrap();
 }
 
 fn expand(potentials :&[i64]) -> Vec<i128> {
@@ -141,10 +137,15 @@ fn fetch_seed()-> Result<i64, String> {
     Ok(1)
 }
 
+/*
+* Parses the pool size variable out of the args list.
+* Defaults to 1 on any invalid input, or no number supplied.
+* Args list is passed in for ease of testing.
+*/
 fn get_pool_size( args: Vec<String>)-> usize {
     if args.len() > 1 {
         let pool_size_str = &args[1];
-        return match &args[1].parse::<usize>() {
+        return match pool_size_str.parse::<usize>() {
           Ok(n) => n,
           Err(_) => 1,
         }
@@ -156,8 +157,7 @@ fn run_pool(seed :i64, pool_size: usize) -> Vec<String> {
     let start = 100*seed;
     let end = 100+start;
     let mut results = Vec::new();
-    results.push(format!("seed:{}",seed));
-    results.push( format!("pool:{}",pool_size));
+    let timer = Instant::now();
     let pool = ThreadPool::new(pool_size);
     let (tx, rx) = channel::<String>();
     for i in 0..pool_size {
@@ -169,15 +169,20 @@ fn run_pool(seed :i64, pool_size: usize) -> Vec<String> {
     drop(tx);
     for received in rx {
         let pr = format!("result: {}", received);
-        print!("{}\n", pr);
         if ! results.contains(&pr){
             results.push(pr);
         }
     }
+    results.push(format!("seed: {}",seed));
+    results.push( format!("pool: {}",pool_size));
+    results.push(format!("time: {}", timer.elapsed().as_millis()));
     return results;
 }
 
 #[cfg_attr(tarpaulin, skip)]
+/*  Untested and no real way of testing it.apart from mocking all the parts.
+*
+*/
 fn main()-> std::io::Result<()> {
     let pool_size = get_pool_size( env::args().collect() );
     let mut file = File::create("log.txt")?;
@@ -206,13 +211,6 @@ fn test_is_valid_true() {
     let sol: [i128; 9] = [1,2,3,4,5,6,7,8,9];
     assert!(is_unique_subset(sol, &nums));
 }
-/*
-#[test]
-fn test_is_valid_false_multiplicity() {
-    let nums = vec![1,2,3,4,5,6,7,8,9,10,11];
-    let sol: [i128; 9] = [1,1,1,1,1,1,1,1,1];
-    assert!(!is_unique_subset(sol, &nums));
-}*/
 
 #[test]
 fn test_is_valid_false() {
@@ -239,6 +237,35 @@ fn test_compute_square_base(){
 fn test_balanced_partition(){
     let result = balanced_partition(12350);
     let solution = vec![12350, 7644, 15704, 5085, 16709, 2400, 17300];
+    assert_eq!(solution, result);
+}
+
+#[test]
+fn test_pool_size_bad_input_defaults(){
+    let mut input = Vec::new();
+    input.push("Johnny".to_string());
+    input.push("Cash".to_string());
+    let solution: usize = 1;
+    let result = get_pool_size(input);
+    assert_eq!(solution, result);
+}
+
+#[test]
+fn test_pool_size_no_input_defaults(){
+    let mut input = Vec::new();
+    input.push("Johnny".to_string());
+    let solution: usize = 1;
+    let result = get_pool_size(input);
+    assert_eq!(solution, result);
+}
+
+#[test]
+fn test_pool_size_good_input_works(){
+    let mut input = Vec::new();
+    input.push("Johnny".to_string());
+    input.push("8".to_string());
+    let solution: usize = 8;
+    let result = get_pool_size(input);
     assert_eq!(solution, result);
 }
 
